7f369286674fa063e627500019b538c7963e4e34
Joshua-Deering <jidjosh2006@icloud.com>
2024-06-25 21:08:34 -0400

    indenets are now spaces

--- Core/Animations.cs
+++ Core/Animations.cs
@@ -7,74 +7,77 @@ namespace Game2D;
 
 public class Animations
 {
-	public SpriteSheet sheet;
-	
-	//the source rectangle for the current animation frame
-	public Rectangle clipRect;
+    public SpriteSheet sheet;
 
-	private double timeSinceFrame;
-	private int curAnim;
-	private int curFrame = 0;
-	//x/y index of the current animation frame
-	private int x;
-	private int y;
+    //the source rectangle for the current animation frame
+    public Rectangle clipRect;
 
-	public Animations(GameServiceContainer services, string img, List<(int fps, int startX, int startY, int numFrames)> anims, int spriteWidth, int spriteHeight)
-	{
-		clipRect = new Rectangle(0, 0, spriteWidth, spriteHeight);
-		curAnim = 0;
-		x = 0;
-		y = 0;
+    private double timeSinceFrame;
+    private int curAnim;
+    private int curFrame = 0;
+    //x/y index of the current animation frame
+    private int x;
+    private int y;
 
-		sheet = new(services, img, anims, spriteWidth, spriteHeight);
-	}
+    public Animations(GameServiceContainer services, string img, List<(int fps, int startX, int startY, int numFrames)> anims, int spriteWidth, int spriteHeight)
+    {
+        clipRect = new Rectangle(0, 0, spriteWidth, spriteHeight);
+        curAnim = 0;
+        x = 0;
+        y = 0;
 
-	public void Update(GameTime gameTime)
-	{
-		timeSinceFrame += gameTime.ElapsedGameTime.TotalNanoseconds;
-		var (startX, startY, numFrames, timePerFrame) = sheet.anims[curAnim];
+        sheet = new(services, img, anims, spriteWidth, spriteHeight);
+    }
 
-		//update frame (skip frames if needed)
-		while (timeSinceFrame > timePerFrame){
-			curFrame++;
+    public void Update(GameTime gameTime)
+    {
+        timeSinceFrame += gameTime.ElapsedGameTime.TotalNanoseconds;
+        var (startX, startY, numFrames, timePerFrame) = sheet.anims[curAnim];
 
-			if(curFrame >= numFrames) {
-				x = startX;
-				y = startY;
-				curFrame = 0;
-			}
-			x = curFrame % sheet.sheetWidth;
-			y = curFrame / sheet.sheetWidth;
+        //update frame (skip frames if needed)
+        while (timeSinceFrame > timePerFrame)
+        {
+            curFrame++;
 
-			clipRect.X = x * sheet.spriteWidth + 1;
-			clipRect.Y = y * sheet.spriteHeight + 1;
-			timeSinceFrame -= timePerFrame;
-		}
-	}
+            if (curFrame >= numFrames)
+            {
+                x = startX;
+                y = startY;
+                curFrame = 0;
+            }
+            x = curFrame % sheet.sheetWidth;
+            y = curFrame / sheet.sheetWidth;
+
+            clipRect.X = x * sheet.spriteWidth + 1;
+            clipRect.Y = y * sheet.spriteHeight + 1;
+            timeSinceFrame -= timePerFrame;
+        }
+    }
 }
 
 public struct SpriteSheet
 {
-	public Texture2D img;
-	public int spriteWidth;
-	public int spriteHeight;
+    public Texture2D img;
+    public int spriteWidth;
+    public int spriteHeight;
 
-	//number of columns in this spritesheet
-	public int sheetWidth;
+    //number of columns in this spritesheet
+    public int sheetWidth;
 
-	public List<(int startX, int startY, int numFrames, int timePerFrame)> anims;
+    public List<(int startX, int startY, int numFrames, int timePerFrame)> anims;
 
-	public SpriteSheet(GameServiceContainer services, string img, List<(int fps, int startX, int startY, int numFrames)> anims, int spriteWidth, int spriteHeight)
-	{
-		this.img = services.GetService<ContentManager>().Load<Texture2D>(img);
-		this.spriteWidth = spriteWidth;
-		this.spriteHeight = spriteHeight;
+    public SpriteSheet(GameServiceContainer services, string img, List<(int fps, int startX, int startY, int numFrames)> anims, int spriteWidth, int spriteHeight)
+    {
+        this.img = services.GetService<ContentManager>().Load<Texture2D>(img);
+        this.spriteWidth = spriteWidth;
+        this.spriteHeight = spriteHeight;
 
-		sheetWidth = this.img.Width / spriteWidth;
+        sheetWidth = this.img.Width / spriteWidth;
 
-		this.anims = [];
-		foreach (var (fps, startX, startY, numFrames) in anims) {
-			this.anims.Add((startX, startY, numFrames, (int)1e9/fps));
-		}
-	}
+        this.anims = [];
+        foreach (var (fps, startX, startY, numFrames) in anims)
+        {
+            this.anims.Add((startX, startY, numFrames, (int)1e9 / fps));
+        }
+    }
 }
